876e0da621e073f57fdfb0232de5bd555245221f
AnhCao2218 <caodnhe150776@fpt.edu.vn>
2024-05-07 10:15:12 +0700

    Initial commit

---
+++ MyApplication/app/src/main/java/com/caodnhe150776/myapplication/slot1/Slot1MainActivity.java
@@ -0,0 +1,32 @@
+package com.caodnhe150776.myapplication.slot1;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.caodnhe150776.myapplication.R;
+
+public class Slot1MainActivity extends AppCompatActivity {
+    TextView tv1;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_slot1_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        tv1=findViewById(R.id.sl1Txt1);
+        tv1.setText("Hello World");
+    }
+}
