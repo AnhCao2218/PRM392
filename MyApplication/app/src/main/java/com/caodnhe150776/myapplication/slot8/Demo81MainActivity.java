b8fd6664fa9e3c064c3586857fb1efe056ac0c54
AnhCao2218 <caodnhe150776@fpt.edu.vn>
2024-06-04 20:56:07 +0700

    slot8
    Exr8

---
+++ MyApplication/app/src/main/java/com/caodnhe150776/myapplication/slot8/Demo81MainActivity.java
@@ -0,0 +1,33 @@
+package com.caodnhe150776.myapplication.slot8;
+
+import android.annotation.SuppressLint;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.caodnhe150776.myapplication.R;
+
+public class Demo81MainActivity extends AppCompatActivity {
+    TextView tv1;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_demo81_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        tv1=findViewById(R.id.demo81tv1);
+        Typeface font= Typeface.createFromAsset(getAssets(),"Blazed.ttf");
+        tv1.setTypeface(font);
+    }
+}
