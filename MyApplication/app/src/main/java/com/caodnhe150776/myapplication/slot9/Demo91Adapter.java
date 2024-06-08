a5741066a82abfffff281fa8d9da6c63c7b61957
AnhCao2218 <caodnhe150776@fpt.edu.vn>
2024-06-08 10:19:19 +0700

    slot9
    Exr

---
+++ MyApplication/app/src/main/java/com/caodnhe150776/myapplication/slot9/Demo91Adapter.java
@@ -0,0 +1,76 @@
+package com.caodnhe150776.myapplication.slot9;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.caodnhe150776.myapplication.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class Demo91Adapter extends BaseAdapter {
+    private Context context;
+    private List<Product91> mList;
+
+    public Demo91Adapter() {
+    }
+
+    public Demo91Adapter(Context context, List<Product91> mList) {
+        this.context = context;
+        this.mList = mList;
+    }
+
+    @Override
+    public int getCount() {
+        return mList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHoler91 holer;
+        if(convertView==null){
+            convertView= LayoutInflater.from(context).inflate(R.layout.demo91_itemview,parent,false);
+            holer=new ViewHoler91();
+            holer.imageView=convertView.findViewById(R.id.demo91_itemview_searchImage);
+            holer.styleIdTV=convertView.findViewById(R.id.demo91_itemview_TvStyleid);
+            holer.brandTV=convertView.findViewById(R.id.demo91_itemview_TvBrand);
+            holer.priceTV=convertView.findViewById(R.id.demo91_itemview_TvPrice);
+            holer.infoTV=convertView.findViewById(R.id.demo91_itemview_TvInfo);
+            convertView.setTag(holer);
+        }
+        else {
+            holer=(ViewHoler91) convertView.getTag();
+        }
+        Product91 product= mList.get(position);
+        if(product!=null){
+
+            Picasso.get().load(product.getSearchImage()).into(holer.imageView);
+            holer.styleIdTV.setText(product.getStyleId());
+            holer.brandTV.setText(product.getBrand());
+            holer.priceTV.setText(product.getPrice());
+            holer.infoTV.setText(product.getInfo());
+
+        }
+        return convertView;
+    }
+
+    static class ViewHoler91{
+        ImageView imageView;
+        TextView styleIdTV,brandTV,priceTV,infoTV;
+    }
+}
---
+++ MyApplication/app/src/main/java/com/caodnhe150776/myapplication/slot9/Demo91MainActivity.java
@@ -0,0 +1,96 @@
+package com.caodnhe150776.myapplication.slot9;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.caodnhe150776.myapplication.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpRetryException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Demo91MainActivity extends AppCompatActivity {
+    private ListView listView;
+    private Demo91Adapter adapter;
+    private List<Product91> list;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_demo91_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        listView=findViewById(R.id.demo91_listview);
+        list= new ArrayList<>();
+        adapter= new Demo91Adapter(this,list);
+        listView.setAdapter(adapter);
+        new FetchProduct91().execute();
+    }
+    private class FetchProduct91 extends AsyncTask<Void, Void,String>{
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            StringBuilder response= new StringBuilder();
+            try{
+                URL url= new URL("http://10.33.11.200:8080/aphp/select.php");
+                HttpURLConnection connection=(HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                BufferedReader reader= new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String line;
+                while ((line=reader.readLine())!=null){
+                    response.append(line);
+                }
+                reader.close();
+            }catch (MalformedURLException e){
+                throw new RuntimeException(e);
+            }catch (IOException e){
+                throw new RuntimeException(e);
+            }
+            return response.toString();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            if(s!=null&& !s.isEmpty()){
+                try {
+                    JSONObject jsonObject =  new JSONObject(s);
+                    JSONArray productArray= jsonObject.getJSONArray("products");
+                    for(int i=0; i<productArray.length();i++){
+                        JSONObject pObject= productArray.getJSONObject(i);
+                        String styleId=pObject.getString("styleid");
+                        String brand=pObject.getString("brands_filter_facet");
+                        String price=pObject.getString("price");
+                        String info=pObject.getString("product_additional_info");
+                        String searchImage=pObject.getString("search_image");
+                        Product91 product91= new Product91(styleId,brand,price,info,searchImage);
+                        list.add(product91);
+                    }
+                    adapter.notifyDataSetChanged();
+                }catch (JSONException e){
+                    throw new RuntimeException(e);
+                }
+
+            }
+        }
+    }
+}
